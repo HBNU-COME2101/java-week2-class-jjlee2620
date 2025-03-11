b08d24a4f398107cc541859b5863bd0e77ec0736
Lee Jungjae <jjlee2620@gmail.com>
2025-03-11 16:47:39 +0900

    Init

---
+++ src/week2/chapter1_2.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class chapter1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("20241030 이정재");
+	}
+}
---
+++ src/week2/chapter1_5.java
@@ -0,0 +1,30 @@
+package week2;
+
+import java.util.Scanner;
+
+public class chapter1_5 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int age = -1;
+		
+		while (age <= 0) {
+			System.out.print("나이 >> ");
+			age = sc.nextInt();
+			
+			if (age <= 0) {
+				System.out.print("나이는 양수로만 입력하세요.\n");
+			}
+		}
+
+		int red = age / 10;
+        int blue = (age%10) / 5;
+        int yellow = age % 5;
+        int result = red + blue + yellow;
+        
+        System.out.print("빨간 초 " + red + "개, 파란 초 " + blue + "개, 노란 초 " + yellow + "개. 총 " + result + "개가 필요합니다.");
+        
+        sc.close();
+	}
+}
